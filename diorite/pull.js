222b75a4b74ebc9d6f0254b470d01a0465367621
Scotty Smith <chssmith@roanoke.edu>
2020-08-04 11:19:30 -0400

    Diorite pull action complete

---
+++ diorite/pull.js
@@ -0,0 +1,19 @@
+const core     = require('@actions/core');
+const github   = require('@actions/github');
+const ( exec ) = require('child_process');
+
+try {
+  const path = core.getInput('directory');
+  exec ('git_pull.sh ' + path,
+        (err, stdout, stderr) => {
+          if (err) {
+            console.error (err);
+          } else {
+            console.log(`stdout: ${stdout}`);
+            console.log(`stderr: ${stderr}`);
+          }
+        }
+  );
+} catch (error) {
+  core.setFailed(error.message);
+}
